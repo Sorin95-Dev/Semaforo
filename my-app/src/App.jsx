5d71ee66c1c2c5efbb6d4c8ccbbb6cb00a1faf7d
sorin oprean petru <sorinoprean@hotmail.es>
2025-11-14 14:30:19 +0100

    Semaforo Final

---
+++ my-app/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import './App.css'
+
+function TrafficLight() {
+  const [color, setColor] = useState("red");
+
+  return (
+    <div className="traffic-light">
+      <div 
+        className={`light red ${color === "red" ? "glow" : ""}`}
+        onClick={() => setColor("red")}
+      ></div>
+      <div 
+        className={`light yellow ${color === "yellow" ? "glow" : ""}`}
+        onClick={() => setColor("yellow")}
+      ></div>
+      <div 
+        className={`light green ${color === "green" ? "glow" : ""}`}
+        onClick={() => setColor("green")}
+      ></div>
+    </div>
+  )
+}
+
+export default TrafficLight
---
+++ my-app/src/main.jsx
@@ -0,0 +1,8 @@
+import { StrictMode } from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import TrafficLight from './App.jsx'
+
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+
+root.render(<TrafficLight />);
